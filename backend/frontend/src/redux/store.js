a5ef14b82fd92904cc1ed605099e201dd35cbd19
mor meir <123agmor@gmail.com>
2023-06-27 16:03:01 +0300

    change folder name

--- backend/frontend/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import imagesReducer from './imagesSlice'
-
-
-export default configureStore({
-    reducer: {
-        images: imagesReducer
-    }
-})
